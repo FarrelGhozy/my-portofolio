83c09ef92bdedce311c0fff104185c730588e16e
FarrelGhozy <farrelafif05@gmail.com>
2026-06-14 11:08:34 +0700

    tambahan github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://farrelghozy.github.io',
+  base: '/my-portofolio',
   server: {
     host: true
   },
